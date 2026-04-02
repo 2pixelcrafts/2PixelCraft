43365081d266da41332a87a9b9f71f6a6797633d
Taman Patel <taman.hansalia@gmail.com>
2026-04-02 11:42:00 +0530

    brave logo added

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,11 @@ export const metadata: Metadata = {
   },
   manifest: "/site.webmanifest",
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
     shortcut: "/favicon.ico",
     apple: "/apple-icon.png",
   },
